df91efad6b20f838040134a2f60e7c553074e484
hmooios <135945583+hmooios@users.noreply.github.com>
2023-12-04 20:24:23 +0630

    Image picker

--- FavDishes/FavDishes/ImagePicker.swift
+++ FavDishes/FavDishes/ImagePicker.swift
@@ -4,3 +4,51 @@
 //
 //  Created by Hmoo Myat Theingi on 03/12/2023.
 //
+
+
+import Foundation
+import UIKit
+import SwiftUI
+
+struct ImagePicker:UIViewControllerRepresentable{
+    
+    @Binding var selectedImage:UIImage?
+    @Binding var isPickerShowing:Bool
+    
+    func makeUIViewController(context: Context) -> UIViewController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = context.coordinator
+        return imagePicker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        //
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+}
+
+class Coordinator:NSObject,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    var parent : ImagePicker
+    init(_ picker: ImagePicker) {
+        self.parent = picker
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print("Image selected")
+        
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            DispatchQueue.main.async{
+                self.parent.selectedImage = image
+            }
+        }
+        parent.isPickerShowing = false
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("Cancelled")
+        parent.isPickerShowing = false
+
+    }
+}
