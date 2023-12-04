c9243faf357ac43e4d8501519b16419ca60f14b7
hmooios <135945583+hmooios@users.noreply.github.com>
2023-12-04 20:26:58 +0630

    Adding PhotoPicker,TextFields,Save features

--- FavDishes/FavDishes/NewDishForm.swift
+++ FavDishes/FavDishes/NewDishForm.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct NewDishForm: View {
+    @Environment(\.dismiss) var dismiss
+    @State private var name=""
+    @State private var restaurant=""
+    
+    @State private var isPickerShowing = false
+    @State private var selectedImage:UIImage?
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            VStack{
+                
+                VStack{
+                    if selectedImage != nil{
+                        Image(uiImage: selectedImage!)
+                            .resizable()
+                            .frame(width: 200,height: 200)
+                    }
+                    Button(action: {
+                        isPickerShowing = true
+                    }, label: {
+                        Text("Select photo")
+                            .foregroundColor(.white)
+                            .font(.title3.bold())
+                            .padding()
+                            .background(.blue)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    })
+                }
+                .sheet(isPresented: $isPickerShowing, content: {
+                    ImagePicker(selectedImage: $selectedImage, isPickerShowing: $isPickerShowing)
+                })
+                TextField("Name",text: $name)
+                TextField("Restaurant", text: $restaurant)
+                
+                Button(action: {
+                    dismiss()
+                }, label: {
+                    Text("Save")
+                        .font(.title.bold())
+                })
+            }.navigationTitle("New dish")
+        }
     }
 }
 
