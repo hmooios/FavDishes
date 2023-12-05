edbb16fea933b2c9b6893e3f9d437237671b3be3
hmooios <135945583+hmooios@users.noreply.github.com>
2023-12-05 16:46:05 +0630

    Complete NewDishForm.swift

--- FavDishes/FavDishes/NewDishForm.swift
+++ FavDishes/FavDishes/NewDishForm.swift
@@ -8,11 +8,101 @@
 import SwiftUI
 
 struct NewDishForm: View {
+    @Environment(\.dismiss) var dismiss
+    @State private var name = ""
+    @State private var restaurant = ""
+    
+    @State private var isPickerShowing = false
+    @State private var selectedImage: UIImage?
+    
+    @ObservedObject var dishes: Dishes
+    
+    @State private var isShowingAlert = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                VStack {
+                    if let image = selectedImage {
+                        Image(uiImage: image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .clipped()
+
+                    } else {
+                        Image(systemName: "photo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200, height: 200)
+                            .cornerRadius(10)
+
+                    }
+                    
+                    Button(action: {
+                        isPickerShowing = true
+                    }, label: {
+                        Text("Select Photo")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    })
+                    Spacer()
+                }
+                .sheet(isPresented: $isPickerShowing) {
+                    ImagePicker(selectedImage: $selectedImage, isPickerShowing: $isPickerShowing)
+                }
+                .padding(.vertical, 20)
+                
+                TextField("Dish Name", text: $name)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal)
+                
+                TextField("Restaurant", text: $restaurant)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal)
+                
+                Button(action: {
+                               
+                               if name.isEmpty && restaurant.isEmpty {
+                                   isShowingAlert = true
+                               } else {
+                                   let newDish = Dish(name: name, imageData: selectedImage?.pngData() ?? Data(), restaurant: restaurant)
+                                   dishes.addDish(newDish)
+                                   
+                                   // Optionally, clear the form fields
+                                   name = ""
+                                   restaurant = ""
+                                   selectedImage = nil
+                                   
+                                   dismiss()
+                               }
+                           }, label: {
+                               Text("Save Dish")
+                                   .font(.headline)
+                                   .foregroundColor(.white)
+                                   .padding()
+                                   .frame(maxWidth: .infinity)
+                                   .background(Color.blue)
+                                   .cornerRadius(10)
+                           })
+                           .padding(.horizontal)
+                       }
+                       .navigationTitle("Add New Dish")
+                       .padding()
+                       .alert(isPresented: $isShowingAlert) {
+                           Alert(
+                               title: Text("Fill the form"),
+                               message: Text("You must fill menu and restaurant"),
+                               dismissButton: .default(Text("I got it"))
+                           )
+                       }
+                   }
+        
+        .navigationViewStyle(StackNavigationViewStyle()) // Use this line for better iPad support
     }
 }
-
 #Preview {
-    NewDishForm()
+    ContentView()
 }
