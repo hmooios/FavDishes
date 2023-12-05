6307679a314758d751ddd519ce2d7f1cacc247d5
hmooios <135945583+hmooios@users.noreply.github.com>
2023-12-05 16:47:33 +0630

    Complete Dishes.swift

--- FavDishes/FavDishes/Dishes.swift
+++ FavDishes/FavDishes/Dishes.swift
@@ -7,14 +7,48 @@
 
 import SwiftUI
 
-class Dishes:ObservableObject{
+class Dishes: ObservableObject {
     @Published var dishes = [Dish]()
-    
-    init(){
+
+    init() {
         loadDishes()
     }
-    
-    func loadDishes(){
-        
+
+    private var fileURL: URL {
+        let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        return documentDirectory.appendingPathComponent("dishes.json")
+    }
+
+    func addDish(_ dish: Dish) {
+        dishes.append(dish)
+        saveDishes()
+    }
+
+    func deleteDish(at indexSet: IndexSet) {
+           guard !indexSet.isEmpty else {
+               return
+           }
+
+           dishes.remove(atOffsets: indexSet)
+           saveDishes()
+       }
+
+
+    func loadDishes() {
+        do {
+            let data = try Data(contentsOf: fileURL)
+            dishes = try JSONDecoder().decode([Dish].self, from: data)
+        } catch {
+            print("Error loading dishes: \(error)")
+        }
+    }
+
+    func saveDishes() {
+        do {
+            let data = try JSONEncoder().encode(dishes)
+            try data.write(to: fileURL)
+        } catch {
+            print("Error saving dishes: \(error)")
+        }
     }
 }
