15deda4b0d11cdabe0519bdd986b00843e5752e7
hmooios <135945583+hmooios@users.noreply.github.com>
2023-12-05 16:47:14 +0630

    Complete Dish.swift

--- FavDishes/FavDishes/Dish.swift
+++ FavDishes/FavDishes/Dish.swift
@@ -6,11 +6,20 @@
 //
 
 import SwiftUI
-struct Dish:Identifiable{
-    var id=UUID()
-    var name:String
-    var image:UIImage
-    var restaurant:String
-    
-    static let example = Dish(name: "Unknown name", image: UIImage(systemName: "photo")!, restaurant: "Unknown restaurant")
+
+struct Dish: Identifiable, Codable {
+    var id = UUID()
+    var name: String
+    var imageData: Data // Change the image property to Data
+    var restaurant: String
+
+    var image: UIImage {
+        guard let uiImage = UIImage(data: imageData) else {
+            // Return a placeholder image or handle the error as needed
+            return UIImage(systemName: "photo")!
+        }
+        return uiImage
+    }
+
+    static let example = Dish(name: "Unknown name", imageData: Data(), restaurant: "Unknown restaurant")
 }
